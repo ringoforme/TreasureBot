104e4364c7184dab49567fa295b668fe5e316847
ringoforme <ringo1for1me@gmail.com>
2024-01-24 20:23:31 +0100

    initial commit

---
+++ backend/src/main/java/com/siwPresent/demo/TreasureBotApplication.java
@@ -0,0 +1,13 @@
+package com.siwPresent.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreasureBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreasureBotApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/siwPresent/demo/TreasureBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siwPresent.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreasureBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
